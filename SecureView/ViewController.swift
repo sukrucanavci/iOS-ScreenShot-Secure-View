bc5ecec6eac9e9a0b58709b8fa4f52d8911db05e
Şükrü Can AVCI <SukruCan.Avci@asiselektronik.com.tr>
2024-01-21 13:25:33 +0300

    Initial Commit

---
+++ SecureView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecureView
+//
+//  Created by Şükrü Can Avcı on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
